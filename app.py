370744bd81f5b250f5985e85faef6dc71f4a26a2
datsuuu-momenn <datsugaketsu@gmail.com>
2024-07-31 22:04:25 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from components.chat_interface_standard import ChatInterfaceStandard
+from components.chat_interface_streaming import ChatInterfaceStreaming
+from components.sidebar import sidebar
+from config.config import Config
+from llm.bedrock_client import BedrockClient
+
+
+def main():
+    st.set_page_config(page_title="Bedrock Chatbot", layout="wide")
+
+    cfg = Config()
+    cfg = sidebar(cfg)
+    bedrock_client = BedrockClient(cfg.region)
+
+    if cfg.use_streaming:
+        chat_interface = ChatInterfaceStreaming(bedrock_client, cfg)
+    else:
+        chat_interface = ChatInterfaceStandard(bedrock_client, cfg)
+    chat_interface.run()
+
+
+if __name__ == "__main__":
+    main()
